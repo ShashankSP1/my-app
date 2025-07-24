72dc1d993b91451c4a1e28ccb8810a2f87c0132e
77shashank <shashank.sp@zeksta.com>
2025-07-24 15:09:58 +0530

    some dummy msg

--- app/Main/page.tsx
+++ app/Main/page.tsx
@@ -1,12 +1,17 @@
-export default function Main(){
-    return(
-        <>
-        <div>
-            <p>Hello Main</p>
-            
-            <h2>We start with some basic commands</h2>
-        </div>
-        </>
-    )
+export default function Main() {
+  return (
+    <>
+      <div>
+        <p>Hello Main</p>
 
+        <h2>We start with some basic commands</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+          ducimus ullam illo commodi consequatur? Minus fugit, facere hic
+          aperiam ab labore alias atque mollitia beatae earum odit libero itaque
+          placeat?
+        </p>
+      </div>
+    </>
+  );
 }
