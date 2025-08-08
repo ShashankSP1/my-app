482f4e7af311a3d0c7fcf1fe0b73eba3dcee27b1
77shashank <shashank.sp@zeksta.com>
2025-08-08 17:12:54 +0530

    Created pages for Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Nav from "./Nav/page";
 import Main from "./Main/page";
-
 export default function Home() {
   return (
-    <>
-      <div>
-        <Nav />
-        <Main />
-      </div>
+     <>
+      <Main />
     </>
   );
 }
