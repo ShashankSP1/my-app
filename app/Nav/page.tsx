8a71047c6de6a8f38710d6c4c650068e2f5bdff9
ShashankSP1 <shashankshiva6361@gmail.com>
2025-11-22 17:16:13 +0530

    Updated the Navbar, Home page and Footer with necessary changes

--- app/Nav/page.tsx
+++ app/Nav/page.tsx
@@ -29,16 +29,14 @@ export default function Nav() {
         className={`fixed top-0 left-0 w-full flex justify-between items-center px-3 sm:px-6 py-3 sm:py-4 z-50 shadow-lg backdrop-blur-md transition-all duration-300
   ${
     theme === "dark"
-      ? "bg-gradient-to-r from-blue-950/80 to-indigo-950/80 border-b border-white/10"
-      : "bg-gradient-to-r from-white/80 to-gray-100/70 border-b border-gray-300/30"
+      ? "bg-transparent border-b border-white/10"
+      : "bg-transparent border-b border-gray-300/20"
   }`}
       >
         <img
-          src={
-            theme === "dark" ? "/Developer_dark.png" : "/Developer_light.png"
-          }
+          src="nav-icon.png"
           alt="icon"
-          className="w-36 sm:w-48 md:w-60 h-10 sm:h-12 object-contain cursor-pointer transition-transform duration-300 hover:scale-105"
+          className="w-36 sm:w-48 md:w-60 lg:w-72 h-10 sm:h-12 object-contain cursor-pointer transition-transform duration-300 hover:scale-105"
         />
         {/* Desktop Menu */}
         <ul className="hidden md:flex gap-2 lg:gap-4 items-center">
@@ -51,7 +49,7 @@ export default function Nav() {
                     : "hover:bg-blue-100 hover:text-blue-700"
                 } ${
                   pathname === "/"
-                    ? "font-semibold text-blue-600 dark:text-blue-400"
+                    ? "font-semibold text-rose-600"
                     : ""
                 } `}
               >
@@ -66,7 +64,12 @@ export default function Nav() {
                   theme === "dark"
                     ? "hover:bg-blue-900/80"
                     : "hover:bg-gray-300/80"
-                }`}
+                }
+                ${
+                  pathname === "/About"
+                    ? "font-semibold text-rose-600"
+                    : ""
+                } `}
               >
                 About
               </button>
@@ -79,6 +82,11 @@ export default function Nav() {
                   theme === "dark"
                     ? "hover:bg-blue-900/80"
                     : "hover:bg-gray-300/80"
+                }
+                ${
+                  pathname === "/Project"
+                    ? "font-semibold text-rose-600"
+                    : ""
                 }`}
               >
                 Projects
@@ -92,6 +100,11 @@ export default function Nav() {
                   theme === "dark"
                     ? "hover:bg-blue-900/80"
                     : "hover:bg-gray-300/80"
+                }
+                ${
+                  pathname === "/Blog"
+                    ? "font-semibold text-rose-600"
+                    : ""
                 }`}
               >
                 Blog
@@ -105,6 +118,11 @@ export default function Nav() {
                   theme === "dark"
                     ? "hover:bg-blue-900/80"
                     : "hover:bg-gray-300/80"
+                }
+                ${
+                  pathname === "/Contact"
+                    ? "font-semibold text-rose-600"
+                    : ""
                 }`}
               >
                 Contact
@@ -172,7 +190,9 @@ export default function Nav() {
       {/* Mobile Menu */}
       <div
         className={`fixed right-0 h-full w-3/4 z-40 sm:w-1/2 md:hidden backdrop-blur-lg shadow-2xl transform transition-transform duration-500 ${
-          isOpen ? "visible opacity-100 translate-x-0" : "invisible opacity-0 translate-x-full"
+          isOpen
+            ? "visible opacity-100 translate-x-0"
+            : "invisible opacity-0 translate-x-full"
         }
         `}
       >
